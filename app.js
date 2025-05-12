1a2fe79a3393150cfab25576c51ca95e451d3137
Dominique42 <dominique.matricon@free.fr>
2025-05-12 13:58:13 +0200

    jss jss

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+//francais, histoire geo, musique, maths
 
+let notesLudivine = [12, 13.5, 14, 16.5];
+let somme=0;
+
+//afficher en console la moyenne generale de Ludivine
+
+
+notesLudivine.forEach (note=>{
+somme+=note;
+})
+
+notesLudivine.forEach (note=>{
+    somme +=note;
+
+});
+
+
+console.log ('la moyenne est : ${somme/notesLudivine.length}')
